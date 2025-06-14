ca311489c8b4648099fbf2fadf53b6beba142b2d
suhaibfida <suhaibfida33@outlook.com>
2025-06-14 13:00:13 +0530

    add signup function

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
-function signup() {
-  alert("Signup running");
+async function signup() {
+  console.log("hello");
+  const username = document.getElementById("signU").value;
+  const password = document.getElementById("signP").value;
 
-  const sign = document.getElementById("sign").innerHTML;
+  await axios.post(`http://localhost:${BACKEND_PORT}/user/signup`, {
+    username,
+    password,
+  });
+  alert("Account created");
 }
 function login() {}
